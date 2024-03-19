f5cdd2eac080845163411e163dbe43bdb3a4fb5e
Zellar9181 <157178694+Zellar9181@users.noreply.github.com>
2024-03-19 03:23:45 -0400

    Update Lab15Functions.cpp

--- Lab15Functions.cpp
+++ Lab15Functions.cpp
@@ -3,8 +3,29 @@
 * Gets and validates lottery info from the user and places it in   *
 * reference parameters referencing variables in the main function. *
 *******************************************************************/
-void getInfo(int & pickFrom, int & numPicks){
-
+void getInfo(int & pickFrom, int & numPicks){   
+    pickFrom = 0;
+    cout << "How many balls (1-12) are in the pool to pick from?";
+    while((pickFrom < 1) or (pickFrom > 12)){
+    while(!(cin >>pickFrom)){
+        cout << "Input Error! There must be between 1 and 12 balls."<< endl;
+        cout << "How many balls (1-12) are in the pool to pick from?";
+        cin.clear();
+        cin.ignore(123, '\n');
+    }
+    }
+    
+    numPicks = 0;
+    cout << "How many balls (1-"<< pickFrom <<") will be drawn?";
+    while((numPicks < 1) or (numPicks > pickFrom)){
+    while(!(cin >>numPicks)){
+        cout << "Input Error!"<< endl;
+        cout << "How many balls (1-"<< pickFrom <<") will be drawn?";
+        cin.clear();
+        cin.ignore(123, '\n');
+    }
+    }
+    return numPicks;
 }
 
 /******************************************************************
